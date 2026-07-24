5e3b82f405c2c47a33309fe1cef43ee412b54be0
JPVillasanta <jhonpaulvillasanta937@gmail.com>
2026-07-24 23:49:24 +0800

    initial structure for index page

--- index.php
+++ index.php
@@ -1,4 +1,37 @@
-Test by jp
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-AAAAAAAAAAAAAAA
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WalangBrownout</title>
+    <link rel="stylesheet" href="generalStyle.css">
+</head>
+<body>
+    <header>
+        <h1>WalangBrownout</h1>
+        <nav>
+            
+        </nav>
+
+        <hr>
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+        <hr>
+
+        <p><strong>&copy; 2026 WalangBrownout. All rights reserved.</strong></p>
+    </footer>
+</body>
+</html>
+
+<?php
+
+?>
