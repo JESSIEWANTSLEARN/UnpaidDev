91264e747023d1efb519407fd1f0993d4ca05bf1
JESSIEWANTSLEARN <palaraojessie19@gmail.com>
2026-07-27 19:27:54 +0800

    Configure database DSN and PDO options

--- config.php
+++ config.php
@@ -5,6 +5,12 @@ $username = 'root';
 $password = '';
 $charset = 'utf8mb4';
 
+$dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
 
 
 ?>
