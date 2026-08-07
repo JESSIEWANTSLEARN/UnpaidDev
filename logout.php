67fc2494286b5b4e2b6a402fb130442b4cfc5c94
JESSIEWANTSLEARN <palaraojessie19@gmail.com>
2026-08-07 22:42:10 +0800

    Refactor logout.php to improve session handling and cookie removal

--- logout.php
+++ logout.php
@@ -1,6 +1,30 @@
 <?php
-require_once 'session.php';
+
+session_start();
+
+// Remove all session variables
+$_SESSION = [];
+
+// Remove PHP session cookie
+if (ini_get("session.use_cookies")) {
+
+    $params = session_get_cookie_params();
+
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000,
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"]
+    );
+}
+
+// Completely destroy session
 session_destroy();
-header("Location: login.php");
+
+// Return to login
+header('Location: login.php');
 exit();
 ?>
