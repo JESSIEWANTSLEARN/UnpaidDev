6206aef971dbeda8aea8eaa8334b56848b070956
JESSIEWANTSLEARN <palaraojessie19@gmail.com>
2026-08-15 17:42:49 +0800

    Reorder use statements in web.php for better organization

--- framework/routes/web.php
+++ framework/routes/web.php
@@ -1,6 +1,8 @@
 <?php
-use App\Http\Controllers\SuperAdminController;
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SuperAdminController;
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/', function () {
     return view('welcome');
