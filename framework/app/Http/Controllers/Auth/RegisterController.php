bb8911024e2e618d3e0f08221f37e4ed3ac4f888
JESSIEWANTSLEARN <palaraojessie19@gmail.com>
2026-08-15 17:39:21 +0800

    Add necessary imports to RegisterController for user registration functionality

--- framework/app/Http/Controllers/Auth/RegisterController.php
+++ framework/app/Http/Controllers/Auth/RegisterController.php
@@ -1,10 +1,14 @@
-<?php
 
+<?php
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Mail\OtpMail;
+use App\Models\User;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail
 class RegisterController extends Controller
 {
     //
